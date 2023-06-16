b164e415608fdfce485f9a2fe5e5a5a511809df4
oulaydi <oussama.oulaydi@gmail.com>
2023-06-16 10:53:11 -0700

    Creating 2-print_alphabet.c file
    Write a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Description: ' I sometimes i can't suffer when I can't fall asleep.'
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i <= 122 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
