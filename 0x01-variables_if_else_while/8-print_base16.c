f04624541894947d755b182259792b6d8e6a4e64
oulaydi <oussama.oulaydi@gmail.com>
2023-06-16 17:37:21 -0700

    Creating 8-print_base16.c file
    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Hexadecimal'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97 ; j <= 102 ; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
