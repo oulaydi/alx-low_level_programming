5e7ed98f6041d95de3567d0805c1416882343979
oulaydi <oussama.oulaydi@gmail.com>
2023-06-20 09:44:38 -0700

    Creating main.h file
    Holding my functions here

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,8 +7,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-
-
-
-
-#endif
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
