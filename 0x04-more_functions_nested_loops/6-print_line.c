f9f117ac3e7c1700023b0f7f474f0bc37557b9ea
oulaydi <oussama.oulaydi@gmail.com>
2023-06-22 10:16:27 -0700

    Creating 6-print_line.c file
    Write a function that draws a straight line in the terminal.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,17 @@
  *
  * Return: Always 0 (Success).
 */
-
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0 ; i <= n ; i++)
+		int i;
+
+		for (i = 0 ; i < n ; i++)
 		{
 			_putchar('_');
 		}
