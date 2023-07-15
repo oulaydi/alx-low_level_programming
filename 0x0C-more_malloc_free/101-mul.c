04897d1af5470d2a6e588551a8bb1bcb8811e038
oulaydi <oussama.oulaydi@gmail.com>
2023-07-14 20:23:07 -0700

    Creating 101-mul.c file
    Write a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,6 +9,7 @@
  *
  * Return: no return.
  */
+
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
@@ -41,6 +42,7 @@ void _is_zero(char *argv[])
  *
  * Return: pointer of a char array.
  */
+
 char *_initialize_array(char *ar, int lar)
 {
 	int i = 0;
@@ -59,6 +61,7 @@ char *_initialize_array(char *ar, int lar)
  *
  * Return: length of the number.
  */
+
 int _checknum(char *argv[], int n)
 {
 	int ln;
@@ -81,6 +84,7 @@ int _checknum(char *argv[], int n)
  *
  * Return: 0 - success.
  */
+
 int main(int argc, char *argv[])
 {
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
