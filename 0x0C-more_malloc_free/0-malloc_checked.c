0907cc93a566aeb37bbe4ef2855e28be507b5b99
oulaydi <oussama.oulaydi@gmail.com>
2023-07-14 20:22:01 -0700

    Creating 0-malloc_checked.c file
    Write a function that allocates memory using malloc.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * malloc_checked - allocates memory.
  * @b: amount of bytes.
@@ -7,6 +8,7 @@
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
+
 void *malloc_checked(unsigned int b)
 {
 	char *p;
