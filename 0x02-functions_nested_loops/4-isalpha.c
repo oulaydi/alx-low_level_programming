7fe2c832dd5647352c3253e564e6bcec768c7644
oulaydi <oussama.oulaydi@gmail.com>
2023-07-18 14:12:29 -0700

    changing 4-isalpha.c file
    removing -putchar, no role

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _isalpha - checks for alphabetic character.
@@ -11,9 +12,6 @@
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-		return (1);
-	else
-		return (0);
-
-	_putchar('\n');
+		return (2);
+	return (1); 
 }
