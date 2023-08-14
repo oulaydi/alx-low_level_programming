0c9937a55168ae87acad54bea8bfba21468d888b
oulaydi <oussama.oulaydi@gmail.com>
2023-08-14 12:57:33 -0700

    Creating 4-new_dog file
    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * new_dog - creates a new dog.
@@ -11,64 +10,44 @@
  * Return: struct dog.
  * if fails, returns NULL.
  */
-
-char *_strdup(char *str)
-{
-	char *strdup;
-	int i, j;
-
-	i = j = 0;
-	if (!str)
-	{
-		return (NULL);
-	}
-	while (str[j])
-	{
-		j++;
-	}
-	strdup = malloc((sizeof(char) * j + 1));
-	if (!strdup)
-	{
-		return (NULL);
-	}
-	while (str[i])
-	{
-		strdup[i] = str[i];
-		i++;
-	}
-	strdup[i] = '\0';
-	return (strdup);
-}
-
-/**
- *new_dog - function that creates a new dog
- *@name: name
- *@age: age
- *@owner: owner
- *Return: void
- */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *g = malloc(sizeof(dog_t));
+	dog_t *p_dog;
+	int i, lname, lowner;
 
-	if (!g)
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
 	{
+		free(p_dog);
 		return (NULL);
 	}
-	g->name = _strdup(name);
-	g->age = age;
-	g->owner = _strdup(owner);
-	if (!g->name)
+
+	for (lname = 0; name[lname]; lname++)
+		;
+
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
+
+	if (!(p_dog->name) || !(p_dog->owner))
 	{
-		free(g);
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
 		return (NULL);
 	}
-	if (!g->owner)
-	{
-		free(g->name);
-		free(g);
-		return (NULL);
-	}
-	return (g);
+
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
 }
