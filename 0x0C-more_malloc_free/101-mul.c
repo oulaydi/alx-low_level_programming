05b09b15d4831d82fea743cdbe02fe291e978a0b
oulaydi <oussama.oulaydi@gmail.com>
2023-10-07 05:07:14 -0700

    changing some code

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,90 +1,126 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * print_error - Prints an error message.
- */
-void print_error(void)
-{
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-}
-
-/**
- * is_valid_digit - Checks if a character is a valid digit.
- * @c: The character to check.
- * Return: 1 if it's a digit, 0 otherwise.
- */
-int is_valid_digit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
- * print_result - Prints the result.
- * @result: The result to print.
- */
-void print_result(long result)
-{
-	char buffer[20];
-	int len = 0;
-	int i;
-
-	if (result == 0)
-		_putchar('0');
-	else
-	{
-		while (result)
-		{
-			buffer[len++] = result % 10 + '0';
-			result /= 10;
-		}
-		for (i = len - 1; i >= 0; i--)
-			_putchar(buffer[i]);
-	}
-
-	_putchar('\n');
-}
-
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array of strings representing the arguments
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
- * Return: 0 on success, 98 on incorrect arguments, 98 on non-digit input
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
+{
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
+
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
+}
+
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	long result;
-	int i;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		print_error();
-		return (98);
-	}
-
-	for (i = 1; i < 3; i++)
-	{
-		char *num = argv[i];
-
-		while (*num)
+		if (i < 0)
 		{
-			if (!is_valid_digit(*num))
+			if (addl > 0)
 			{
-				print_error();
-				return (98);
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
-			num++;
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-
-	result = atoll(argv[1]) * atoll(argv[2]);
-	print_result(result);
-
+	printf("%s\n", nout);
 	return (0);
 }
