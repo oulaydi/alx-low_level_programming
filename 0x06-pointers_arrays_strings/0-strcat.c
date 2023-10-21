af5bcad93b442f7278fbbed88f75caea716389db
oulaydi <oussama.oulaydi@gmail.com>
2023-10-21 02:04:50 +0100

    update 0-strcat file
    make file shorter

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0, j = 0;
 
-	i = 0;
-	j = 0;
+	for (; dest[i] != '\0'; i++)
+		;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	while (src[j] != '\0')
-	{
+	for (; src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
-		j++;
-		i++;
-	}
 
 	dest[i] = '\0';
 	return (dest);
