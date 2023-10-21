e0ab805893726fea3788119534c20c4e76f2050e
oulaydi <oussama.oulaydi@gmail.com>
2023-10-21 22:42:02 +0100

    Create 1-strncpy function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	for (; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for (; i < n; i++)
+	for ( ; i < n; i++)
 		dest[i] = '\0';
-		
+
 	return (dest);
 }
